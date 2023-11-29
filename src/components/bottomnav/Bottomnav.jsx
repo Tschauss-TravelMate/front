52ad0370b3a57c438ff88fb6f4b93e86bb97cf42
vvalvvizal <vvalvvizal@gmail.com>
2023-11-30 01:50:38 +0900

    ./image

--- src/components/bottomnav/Bottomnav.jsx
+++ src/components/bottomnav/Bottomnav.jsx
@@ -10,38 +10,62 @@ import { NavLink, useLocation } from "react-router-dom";
 function Bottomnav() {
   const location = useLocation();
 
-  if (location.pathname === '/'){
+  if (location.pathname === "/") {
     return null;
   }
 
   const navItems = [
     {
-      path: '/post',
+      path: "/post",
       icon: (
-        <div className={`${location.pathname === '/post' ? styles.out_circle : ''}`}>
-          <MdPeopleOutline className={`${location.pathname === '/post' ? styles.click_emt : styles.emt1}`}  />
-          {location.pathname === '/post' && <div className={styles.in_circle}></div>}
+        <div
+          className={`${
+            location.pathname === "/post" ? styles.out_circle : ""
+          }`}
+        >
+          <MdPeopleOutline
+            className={`${
+              location.pathname === "/post" ? styles.click_emt : styles.emt1
+            }`}
+          />
+          {location.pathname === "/post" && (
+            <div className={styles.in_circle}></div>
+          )}
         </div>
       ),
-      label: location.pathname === '/post' ? '' : '게시글',
-      style: location.pathname === '/post' ? '' : styles.emt_font
-    },
-    { 
-      path: '/save',
-      icon: (
-        <div className={`${location.pathname === '/save' ? styles.out_circle : ''}`}>
-          <MdBookmarkBorder className={`${location.pathname === '/save' ? styles.click_emt : styles.emt2}`}  />
-          {location.pathname === '/save' && <div className={styles.in_circle}></div>}
-        </div>
-      ),
-      label: location.pathname === '/save' ? '' : '저장',
-      style: location.pathname === '/save' ? '' : styles.emt_font 
+      label: location.pathname === "/post" ? "" : "게시글",
+      style: location.pathname === "/post" ? "" : styles.emt_font,
     },
     {
-      path: '/mainpage',
+      path: "/save",
       icon: (
-        <div className={`${location.pathname === '/mainpage' ? styles.out_circle : ''}`}>
-          {location.pathname === '/mainpage' ? (
+        <div
+          className={`${
+            location.pathname === "/save" ? styles.out_circle : ""
+          }`}
+        >
+          <MdBookmarkBorder
+            className={`${
+              location.pathname === "/save" ? styles.click_emt : styles.emt2
+            }`}
+          />
+          {location.pathname === "/save" && (
+            <div className={styles.in_circle}></div>
+          )}
+        </div>
+      ),
+      label: location.pathname === "/save" ? "" : "저장",
+      style: location.pathname === "/save" ? "" : styles.emt_font,
+    },
+    {
+      path: "/mainpage",
+      icon: (
+        <div
+          className={`${
+            location.pathname === "/mainpage" ? styles.out_circle : ""
+          }`}
+        >
+          {location.pathname === "/mainpage" ? (
             <>
               <WhiteMainicon className={`${styles.main_button}`} />
               <div className={styles.in_circle}></div>
@@ -51,43 +75,59 @@ function Bottomnav() {
           )}
         </div>
       ),
-      label: location.pathname === '/mainpage' ? '' : '홈',
-      style: location.pathname === '/mainpage' ? '' : styles.emt_font
-      
-    },        
-    { 
-      path: '/plan',
-      icon: (
-        <div className={`${location.pathname === '/plan' ? styles.out_circle : ''}`}>
-          <MdCalendarToday className={`${location.pathname === '/plan' ? styles.click_emt : styles.emt3}`}  />
-          {location.pathname === '/plan' && <div className={styles.in_circle}></div>}
-        </div>
-      ),
-      label: location.pathname === '/plan' ? '' : '일정',
-      style: location.pathname === '/plan' ? '' : styles.emt_font
+      label: location.pathname === "/mainpage" ? "" : "홈",
+      style: location.pathname === "/mainpage" ? "" : styles.emt_font,
     },
     {
-      path: '/profile',
+      path: "/plan",
       icon: (
-        <div className={`${location.pathname === '/profile' ? styles.out_circle : ''}`}>
-          <MdOutlinePermIdentity className={`${location.pathname === '/profile' ? styles.click_emt : styles.emt3}`}  />
-          {location.pathname === '/profile' && <div className={styles.in_circle}></div>}
+        <div
+          className={`${
+            location.pathname === "/plan" ? styles.out_circle : ""
+          }`}
+        >
+          <MdCalendarToday
+            className={`${
+              location.pathname === "/plan" ? styles.click_emt : styles.emt3
+            }`}
+          />
+          {location.pathname === "/plan" && (
+            <div className={styles.in_circle}></div>
+          )}
         </div>
       ),
-      label: location.pathname === '/profile' ? '' : '프로필',
-      style: location.pathname === '/profile' ? '' : styles.emt_font
+      label: location.pathname === "/plan" ? "" : "일정",
+      style: location.pathname === "/plan" ? "" : styles.emt_font,
+    },
+    {
+      path: "/profile",
+      icon: (
+        <div
+          className={`${
+            location.pathname === "/profile" ? styles.out_circle : ""
+          }`}
+        >
+          <MdOutlinePermIdentity
+            className={`${
+              location.pathname === "/profile" ? styles.click_emt : styles.emt3
+            }`}
+          />
+          {location.pathname === "/profile" && (
+            <div className={styles.in_circle}></div>
+          )}
+        </div>
+      ),
+      label: location.pathname === "/profile" ? "" : "프로필",
+      style: location.pathname === "/profile" ? "" : styles.emt_font,
     },
   ];
-
 
   return (
     <nav className={styles.nav}>
       {navItems.map((item, index) => (
         <div key={index}>
           <NavLink to={item.path} className={styles.nav_link}>
-            <div>
-              {item.icon}
-            </div>
+            <div>{item.icon}</div>
             <div className={`${item.style} ${styles.additionalStyle}`}>
               {item.label}
             </div>
