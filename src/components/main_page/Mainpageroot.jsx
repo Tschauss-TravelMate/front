2200bef0dee6dcc7fdc9684842e6680df8010ffa
Shinji <sjh1803@naver.com>
2023-11-29 22:18:08 +0900

    메인페이지

--- src/components/main_page/Mainpageroot.jsx
+++ src/components/main_page/Mainpageroot.jsx
@@ -1,12 +1,33 @@
 ﻿//import styles from "./Mainpageroot.module.css"
+import { useState, useEffect } from "react";
+import MainpageContent from "./MainpageContent";
+import styles from "./MainpageContent.module.css";
+import axios from "axios";
+import { Back } from "../const/url";
 
 
-function Mainpageroot() {
+const Mainpageroot = () => {
+  const [Home, setPlans] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${Back}/api/Home_get`)
+      .then((response) => {
+        setPlans(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
-       메인
+      <div className={styles.container}>
+        <MainpageContent plans={Home} />
+      </div>
     </div>
   );
-}
+};
 
-export default Mainpageroot;
+export default  Mainpageroot;
