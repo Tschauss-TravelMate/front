91eb9f423fdefcae8bedec22a19a3e63b9c9de0e
vvalvvizal <vvalvvizal@gmail.com>
2023-11-29 15:27:50 +0900

    bottomnav

--- src/components/Bottomnav/Bottomnav.jsx
+++ src/components/Bottomnav/Bottomnav.jsx
@@ -10,28 +10,24 @@ function Bottomnav() {
   return (
     <nav className={styles.nav}>
       <div>
-        <NavLink to="/Post">
+        <NavLink to="/Post" style={{ textDecoration: "none" }}>
           <div>
             <MdPeopleOutline className={styles.emt1} />
           </div>
-          <div className={styles.emt_font} style={{ textDecoration: "none" }}>
-            게시글
-          </div>
+          <div className={styles.emt_font}>게시글</div>
         </NavLink>
       </div>
       <div>
-        <NavLink to="/Save">
+        <NavLink to="/Save" style={{ textDecoration: "none" }}>
           <div>
             <MdBookmarkBorder className={styles.emt2} />
           </div>
-          <div className={styles.emt_font} style={{ textDecoration: "none" }}>
-            저장
-          </div>
+          <div className={styles.emt_font}>저장</div>
         </NavLink>
       </div>
       <div>
         <div className={styles.out_circle}>
-          <NavLink to="/Mainpage">
+          <NavLink to="/Mainpage" style={{ textDecoration: "none" }}>
             <div className={styles.main_button}>
               <Mainicon />
             </div>
@@ -40,23 +36,19 @@ function Bottomnav() {
         </div>
       </div>
       <div>
-        <NavLink to="/Plan">
+        <NavLink to="/Plan" style={{ textDecoration: "none" }}>
           <div>
             <MdCalendarToday className={styles.emt3} />
           </div>
-          <div className={styles.plan_font} style={{ textDecoration: "none" }}>
-            일정
-          </div>
+          <div className={styles.plan_font}>일정</div>
         </NavLink>
       </div>
       <div>
-        <NavLink to="/Profile">
+        <NavLink to="/Profile" style={{ textDecoration: "none" }}>
           <div>
             <MdOutlinePermIdentity className={styles.emt4} />
           </div>
-          <div className={styles.emt_font} style={{ textDecoration: "none" }}>
-            프로필
-          </div>
+          <div className={styles.emt_font}>프로필</div>
         </NavLink>
       </div>
     </nav>
