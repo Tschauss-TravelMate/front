24cfe3474b89808e04e4b1dcc38c9ea9b315cfa2
hyeon0820 <13whdduq13@naver.com>
2023-12-03 19:22:26 +0900

    planCreate_V4

--- src/components/plan_page/plan_pageCreate/Header.jsx
+++ src/components/plan_page/plan_pageCreate/Header.jsx
@@ -1,11 +1,26 @@
-//import styles from "./Header.module.css"
+import styles from "./Header.module.css"
 import Bracket from "../image/bracket.svg?component";
+import { MdOutlineSearch } from "react-icons/md";
 
 function Header() {
     return (
         <div>
-            <Bracket />
-            <div>여행,어디로 떠나시나요?</div>
+            <div className={styles.header_location}>
+                <div className={styles.plan_font}>
+                    <div>
+                        <Bracket />{'   '}
+                        <input type="text" placeholder="여행, 어디로 떠나시나요?" className={styles.city_input} />
+                    </div>
+                </div>
+                <div><MdOutlineSearch className={styles.search} /></div>
+            </div>
+            <div className={styles.font_position}>
+                <div className={styles.header_font}>
+                    국내도시
+                    <div className={styles.select_box}></div>
+                </div>
+                <div className={styles.header_font}>해외도시</div>
+            </div>
         </div>
     );
   }
